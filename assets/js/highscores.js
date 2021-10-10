86ce92a4a526629087a3c7f02b3651fd5ee96331
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-10 03:20:10 +0100

    start forEach setup

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,25 +1,30 @@
 const clearScoresBtn = document.querySelector("#clear-scores");
 const highScoresList = document.querySelector("#high-scores-list");
 
-// get scores/initials from LS
+// get scores/initials from LS (array of objects)
 const getScoreData = function () {
   const highScoresFromLS = JSON.parse(localStorage.getItem("highScores"));
   console.log(highScoresFromLS);
+
+  const getWinnerDetails = function () {};
+
+  highScoresList.forEach(getWinnerDetails);
 };
+
+// get score value from array object & declare as variable?
+
+// get initials string from array object & declare as variable?
 
 // create & append to high scores list
-const renderScoresList = function (myScoresArray) {
-  const constructScoresList = function (eachInitials, initialsIndex) {
-    const scoreListItem = document.createElement("li");
-    scoreListItem.setAttribute("class", "scores-list-item");
-    scoreListItem.textContent = eachInitials;
-    highScoresList.appendChild(scoreListItem);
-  };
-  highScoresList.innerHTML = "";
-
-  //   insert forEach on initialsFromLS
-  myScoresArray.forEach(constructScoresList);
-};
+// const renderScoresList = function (myScoresArray) {
+//     const constructScoresList = function () {
+//       const scoreListItem = document.createElement("li");
+//       scoreListItem.setAttribute("class", "scores-list-item");
+//       scoreListItem.textContent = ;
+//       highScoresList.appendChild(scoreListItem);
+//     };
+//     highScoresList.innerHTML = "";
+// };
 
 // clear high scores
 const clearHighScores = function () {
@@ -32,6 +37,7 @@ const clearHighScores = function () {
 
 // window load event listener
 window.addEventListener("load", getScoreData);
+// window.addEventListener("load", renderScoresList);
 
 // clear scores click event
 clearScoresBtn.addEventListener("click", clearHighScores);
